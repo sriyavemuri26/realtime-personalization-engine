afb798aa0d2bdeef32d5225e1aa660ec3b9d5326
sriyavemuri26 <ksv2630@gmail.com>
2026-09-07 19:53:44 -0400

    fix: added analytics table

--- terraform/lambda_serving.tf
+++ terraform/lambda_serving.tf
@@ -16,6 +16,22 @@ resource "aws_iam_role" "serving_lambda_role" {
     }]
   })
 }
+resource "aws_dynamodb_table" "kuairand_analytics" {
+  name         = "kuairand_analytics"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "user_id"
+  range_key    = "video_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "video_id"
+    type = "S"
+  }
+}
 
 resource "aws_iam_role_policy_attachment" "serving_lambda_logs" {
   role       = aws_iam_role.serving_lambda_role.name
@@ -30,22 +46,31 @@ resource "aws_iam_role_policy" "serving_lambda_dynamo_policy" {
     Version = "2012-10-17"
     Statement = [{
       Effect   = "Allow"
-      Action   = ["dynamodb:GetItem"]
-      Resource = aws_dynamodb_table.user_profiles.arn
+      Action   = [
+        "dynamodb:GetItem",
+        "dynamodb:Query",
+        "dynamodb:Scan"
+      ]
+      Resource = [
+        "arn:aws:dynamodb:us-east-2:783598594659:table/user_profiles",
+        "arn:aws:dynamodb:us-east-2:783598594659:table/kuairand_analytics"
+      ]
     }]
   })
 }
 
 resource "aws_lambda_function" "serving_lambda" {
-  function_name = "recommendation-serving-lambda"
-  role          = aws_iam_role.serving_lambda_role.arn
-  handler       = "serving.lambda_handler"
-  runtime       = "python3.11"
-  filename      = data.archive_file.serving_lambda_zip.output_path
+  function_name    = "recommendation-serving-lambda"
+  role             = aws_iam_role.serving_lambda_role.arn
+  handler          = "serving.lambda_handler"
+  runtime          = "python3.11"
+  filename         = data.archive_file.serving_lambda_zip.output_path
+  source_code_hash = data.archive_file.serving_lambda_zip.output_base64sha256
 
   environment {
     variables = {
-      USER_PROFILES_TABLE = aws_dynamodb_table.user_profiles.name
+      USER_PROFILES_TABLE      = aws_dynamodb_table.user_profiles.name
+      KUAIRAND_ANALYTICS_TABLE = aws_dynamodb_table.kuairand_analytics.name
     }
   }
 
